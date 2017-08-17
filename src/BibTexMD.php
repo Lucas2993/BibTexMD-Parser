3ecf712749dfc7f653655400398a6abcdc192747
LucasAbella29 <lucas.gabriel2993@gmail.com>
2017-08-17 00:42:44 -0300

    Agregando dependencias
    - Se agregaron al archivo "composer.json" las dependencias necesarias para el funcionamiento.

--- src/BibTexMD.php
+++ src/BibTexMD.php
@@ -1,6 +1,6 @@
 <?php
 
-use erusev\Parsedown;
+use erusev\parsedown\Parsedown;
 
 class BibTexMD extends Parsedown {
 
@@ -11,17 +11,14 @@ class BibTexMD extends Parsedown {
   }
 
   protected function inlineBibTex($excerpt){
-    $regular_expression = '/^{c:([#\w]\w+)}(.*?){\/c}/';
+    $regular_expression = '/\@(\w+)\{(?:[^}{]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}/';
 
     if (preg_match($regular_expression, $excerpt['text'], $matches)){
       return array(
         'extent' => strlen($matches[0]),
         'element' => array(
           'name' => 'span',
-          'text' => $matches[2],
-          'attributes' => array(
-            'style' => 'color: ' . $matches[1],
-          ),
+          'text' => 'hola'
         )
       );
     }
